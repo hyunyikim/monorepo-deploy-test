f946b6bbeed50967e8c9362d77358eb2a015a760
Yaeger Moon <60789654+YaegerMoon@users.noreply.github.com>
2022-05-06 15:40:57 +0900

    chore(eslint): ESlint 및 Prettier 설치/설정 (.feat zero-install) (#2)
    Co-authored-by: YaegerMoon <yaeger@mass-adoption.com>

---
+++ .eslintrc.js
@@ -0,0 +1,65 @@
+const path = require('path');
+
+module.exports = {
+	extends: ['prettier'],
+	plugins: ['prettier', 'react-hooks'],
+	env: {
+		browser: true,
+		jest: true,
+		es6: true,
+	},
+	rules: {
+		'prettier/prettier': 'warn',
+
+		'react-hooks/rules-of-hooks': 'error',
+		'react-hooks/exhaustive-deps': 'warn',
+		'react/react-in-jsx-scope': 'off',
+		'react/jsx-props-no-spreading': 'off',
+		'react/function-component-definition': 'off',
+	},
+	overrides: [
+		{
+			files: ['**/*.ts?(x)'],
+			parser: '@typescript-eslint/parser',
+			extends: [
+				'plugin:@typescript-eslint/recommended',
+				'plugin:@typescript-eslint/recommended-requiring-type-checking',
+			],
+			rules: {
+				'@typescript-eslint/no-unused-vars': ['warn'],
+				'react/prop-types': 'off',
+				'react/require-default-props': 'off',
+				'@typescript-eslint/explicit-module-boundary-types': 'off',
+				'no-use-before-define': 'off',
+				'@typescript-eslint/no-use-before-define': [
+					'error',
+					{variables: false},
+				],
+				'no-useless-constructor': 'off',
+				'@typescript-eslint/no-useless-constructor': 'error',
+				'@typescript-eslint/no-floating-promises': 'off',
+			},
+			parserOptions: {
+				project: [
+					'./tsconfig.base.json',
+					'./packages/**/tsconfig.json',
+				],
+			},
+		},
+		{
+			files: [
+				'packages/testmono/**/*.ts?(x)',
+				'packages/testmono/**/*.js?(x)',
+			],
+			settings: {
+				'import/resolver': {
+					node: {
+						project: path.resolve(
+							__dirname + '/packages/testmono/tsconfig.json'
+						),
+					},
+				},
+			},
+		},
+	],
+};
---
+++ packages/testmono/src/button/index.tsx
@@ -0,0 +1,29 @@
+import {forwardRef, PropsWithChildren} from 'react';
+
+export interface PaperProps {
+	className?: string;
+	disablePadding?: boolean;
+	square?: boolean;
+	shadow?: boolean;
+}
+
+const Paper = forwardRef<HTMLDivElement, PropsWithChildren<PaperProps>>(
+	(
+		{
+			className,
+			disablePadding = false,
+			square = false,
+			shadow = false,
+			children,
+		},
+		ref
+	) => {
+		return (
+			<div ref={ref} className={className}>
+				{children}
+			</div>
+		);
+	}
+);
+
+export default Paper;
