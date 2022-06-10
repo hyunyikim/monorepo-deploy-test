d29bf668118cf7b207d7ad6f015d3511ca38d2f7
Yaeger Moon <60789654+YaegerMoon@users.noreply.github.com>
2022-06-11 00:49:26 +0900

    feat(admin-api): 관리자 리스트 조회 api 구현 (#39)

--- packages/admin-api/src/admin/dto/find-admin.dto.ts
+++ packages/admin-api/src/admin/dto/find-admin.dto.ts
@@ -1,17 +1,53 @@
-import {IsDecimal, IsArray, IsJSON} from 'class-validator';
+import {plainToInstance, Transform, Type} from 'class-transformer';
+import {
+	IsDecimal,
+	IsArray,
+	IsJSON,
+	IsNotEmpty,
+	IsEmail,
+	IsString,
+	IsOptional,
+	ValidateNested,
+	IsNumber,
+} from 'class-validator';
 
 export class FindAdminDto {
 	@IsDecimal()
 	readonly idx: number;
 }
 
-export class FindAdminListDto {
-	@IsArray()
-	readonly sort: [string, 'ASC' | 'DESC'];
+export class FindAdminFilter {
+	@IsOptional()
+	@IsString()
+	readonly name: string;
 
-	@IsArray()
+	@IsOptional()
+	@IsEmail()
+	readonly email: string;
+}
+
+export class Range {
+	range: [number, number];
+}
+
+export class FindAdminListDto {
+	// @IsArray()
+	@Transform(({value}) => plainToInstance(Array, JSON.parse(value as string)))
+	@Type(() => Array)
+	@IsNotEmpty()
+	readonly sort: [string, string];
+
+	// @ValidateNested({each: true})
+	@Transform(({value}) => plainToInstance(Array, JSON.parse(value as string)))
+	@Type(() => Array)
+	@IsNotEmpty()
 	readonly range: [number, number];
 
-	@IsJSON()
-	readonly filter: Record<string, string>;
+	@ValidateNested({each: true})
+	@Transform(({value}) =>
+		plainToInstance(FindAdminFilter, JSON.parse(value as string))
+	)
+	@Type(() => FindAdminFilter)
+	@IsNotEmpty()
+	readonly filter: FindAdminFilter;
 }
