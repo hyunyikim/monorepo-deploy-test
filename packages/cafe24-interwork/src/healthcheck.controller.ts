e817b0765b7c8d1d4dec4e4b6fe22b072ed0a3c9
Yaeger Moon <60789654+YaegerMoon@users.noreply.github.com>
2022-07-28 15:58:29 +0900

    feat(cafe24): :fire: healthcheck용 api 변경 (#65)
    Co-authored-by: YaegerMoon <yaeger@mass-adoption.com>

--- packages/cafe24-interwork/src/healthcheck.controller.ts
+++ packages/cafe24-interwork/src/healthcheck.controller.ts
@@ -1,9 +1,20 @@
 import {Controller, Get} from '@nestjs/common';
+import {hostname, networkInterfaces, userInfo, type, arch} from 'os';
 
 @Controller()
 export class HealthCheckController {
+	private devMode: boolean;
+	constructor() {
+		this.devMode = process.env.NODE_ENV == 'development';
+	}
 	@Get('')
 	healthCheck() {
-		return 'CAFE24-INTERWORK';
+		return {
+			hostname: hostname(),
+			userInfo: this.devMode ? userInfo() : undefined,
+			type: this.devMode ? type() : undefined,
+			cpuArchitecture: this.devMode ? arch() : undefined,
+			networkInterfaces: this.devMode ? networkInterfaces() : undefined,
+		};
 	}
 }
