14b41dbb47b7a7c01621af01ed24fb337cb5723e
Yaeger Moon <60789654+YaegerMoon@users.noreply.github.com>
2022-08-19 16:40:00 +0900

    chore(cafe24): deploy 단계에서의 logging 정보 추가 (#88)
    Co-authored-by: YaegerMoon <yaeger@mass-adoption.com>

--- packages/cafe24-interwork/script/start.dev.sh
+++ packages/cafe24-interwork/script/start.dev.sh
@@ -1,21 +1,28 @@
+LOG_POINT=~/log/cafe24-interwork/deploy.log
+
+echo ">[$(date) START DEPLOY BY USER : ${whoami}]" >>  LOG_POINT
+
 eval `ssh-agent -s`;
 ssh-add ~/.ssh/vircle_deploy_key;
 
 cd ~/vircle/packages/cafe24-interwork
 
-echo ">[$(date) PULL LATEST SOURCE FROM GITHUB]" >> ~/log/cafe24-interwork/deploy.log
+echo ">[$(date) PULL LATEST SOURCE FROM GITHUB]" >> LOG_POINT
 
-git pull >>  ~/log/cafe24-interwork/deploy.log
+git pull >>  LOG_POINT
 
 yarn
+
+echo ">[$(date) BUILD START]" >> LOG_POINT
+
 yarn build
 
-echo ">[$(date)] Kill All pm2 process" >> ~/log/cafe24-interwork/deploy.log
+echo ">[$(date)] Kill All pm2 process]" >> LOG_POINT
 
 yarn pm2 kill
 
-echo ">[$(date)] Application Run by pm2" >> ~/log/cafe24-interwork/deploy.log
+echo ">[$(date)] Application Run by pm2]" >>LOG_POINT
 
-NODE_ENV=development yarn pm2 start --name CAFE24-INTERWORK dist/main.js
+NODE_ENV=development yarn pm2 start --name CAFE24-INTERWORK dist/main.js >> LOG_POINT
 
-echo "[$(date)] start on dev mode" >> ~/log/cafe24-interwork/deploy.log
+echo "[$(date)] DONE DEPLOY]" >> LOG_POINT
