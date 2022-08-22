f65b1058e0b223c6e8ba33cd0434c940d6eaf1f5
Yaeger Moon <60789654+YaegerMoon@users.noreply.github.com>
2022-08-22 14:37:08 +0900

    chore(cafe24): 배포 환경에서 환경변수 설정 (#92)

--- packages/cafe24-interwork/script/start.dev.sh
+++ packages/cafe24-interwork/script/start.dev.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
 whoami
 
-LOG_POINT=~/log/cafe24-interwork/deploy.log
+export HOME="/root"
 
-echo LOG_POINT
+echo $HOME
 
-echo ">[$(date) START DEPLOY BY USER : ${whoami}]" >>  LOG_POINT
+LOG_POINT="/root/log/cafe24-interwork/deploy.log"
+
+echo $LOG_POINT
+
+echo ">[$(date) START DEPLOY BY USER : ${whoami}]" >>  $LOG_POINT
 
 eval `ssh-agent -s`;
 ssh-add ~/.ssh/vircle_deploy_key;
 
 cd ~/vircle/packages/cafe24-interwork
 
-echo ">[$(date) PULL LATEST SOURCE FROM GITHUB]" >> LOG_POINT
+echo ">[$(date) PULL LATEST SOURCE FROM GITHUB]" >> $LOG_POINT
 
-git pull >>  LOG_POINT
+git pull >>  $LOG_POINT
 
 yarn
 
-echo ">[$(date) BUILD START]" >> LOG_POINT
+echo ">[$(date) BUILD START]" >> $LOG_POINT
 
 yarn build
 
-echo ">[$(date)] Kill All pm2 process]" >> LOG_POINT
+echo ">[$(date)] Kill All pm2 process]" >> $LOG_POINT
 
-pm2 kill
+yarn pm2 kill
 
-echo ">[$(date)] Application Run by pm2]" >>LOG_POINT
+echo ">[$(date)] Application Run by pm2]" >> $LOG_POINT
 
-NODE_ENV=development pm2 start --name CAFE24-INTERWORK dist/main.js >> LOG_POINT
+NODE_ENV=development yarn pm2 start --name CAFE24-INTERWORK dist/main.js >> $LOG_POINT
 
-echo "[$(date)] DONE DEPLOY]" >> LOG_POINT
+echo "[$(date)] DONE DEPLOY]" >> $LOG_POINT
