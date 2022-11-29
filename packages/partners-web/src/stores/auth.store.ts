4af13b431ad7f4a63e646990bfb1f86c27bed8d9
hyunyikim <hailey@mass-adoption.com>
2022-11-29 10:40:13 +0900

    fix(partners-web): iframe 자식창에서 토큰 localStorage로 관리하지 않도록 수정 (#205)
    * chore(partners-web): updated apps changelog
    
    * fix(partners-web): 목록 조회 필터 중 날짜 입력 직접 가능하도록 수정 (#159)
    
    * fix(partners-web): iframe 자식창에서 토큰 localStorage로 관리하지 않도록 수정 (#204)
    
    부모창에서 넘어온 토큰을 전역상태로 관리한다. (추후 아예 이관했을 때 localStorage로 관리할 예정)
    
    Co-authored-by: Woody Kim <woody@mass-adoption.com>
    Co-authored-by: YaegerMoon <yaeger@mass-adoption.com>
    Co-authored-by: Hwanieee <dawa9293@gmail.com>

--- packages/partners-web/src/stores/auth.store.ts
+++ packages/partners-web/src/stores/auth.store.ts
@@ -9,10 +9,18 @@ interface LoginState {
 }
 
 export const useLoginStore = create<LoginState>((set, get) => ({
-	token: localStorage.getItem(TOKEN_KEY),
-	setLogin: (token: string) => set(() => ({token})),
-	setLogout: () =>
+	// TODO: iframe 전체 옮긴 후  localStorage에 저장하는 것으로 변경
+	// token: localStorage.getItem(TOKEN_KEY),
+	token: null,
+	setLogin: (token: string) =>
+		set(() => {
+			// localStorage.setItem(TOKEN_KEY, token);
+			return {token};
+		}),
+	setLogout: () => {
+		// localStorage.removeItem(TOKEN_KEY);
 		set(() => ({
 			token: null,
-		})),
+		}));
+	},
 }));
