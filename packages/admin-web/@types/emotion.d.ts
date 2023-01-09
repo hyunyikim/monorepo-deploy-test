a20b490f3c7b4dad308d383df18a07a4fb653340
Hyuntae Kim <0gusxo0@naver.com>
2023-01-09 12:01:01 +0900

    어드민 웹 프로젝트 초기화 (#285)
    * fix(admin-web): clear project
    
    Co-authored-by: Woody Kim <woody@mass-adoption.com>

--- packages/admin-web/@types/emotion.d.ts
+++
@@ -1,6 +0,0 @@
-import {Theme as ITheme} from '@vircle/styles';
-
-declare module '@emotion/react' {
-	// eslint-disable-next-line @typescript-eslint/no-empty-interface
-	export interface Theme extends ITheme {}
-}
