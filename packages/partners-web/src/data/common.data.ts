f1b315564f8f843d28dfc35dcabd1b5b2014a608
hyunyikim <hailey@mass-adoption.com>
2023-02-24 18:56:05 +0900

    Hotfix/sxlp 3010 (#415)
    * fix(cafe24-interwork): webhook, 카테고리로 상품 가져오게 수정
    
    * fix(partners-web): 개런티 카드 디자인 업로드 안내문구 수정
    
    ---------
    
    Co-authored-by: hebee935 <hebee935@gmail.com>

--- packages/partners-web/src/data/common.data.ts
+++ packages/partners-web/src/data/common.data.ts
@@ -1,7 +1,6 @@
 export const DATE_FORMAT = 'yyyy-MM-dd';
 export const DATE_FORMAT_SEPERATOR_DOT = 'yyyy.MM.dd';
-export const CARD_DESIGN_GUIDE_LINK =
-	'https://guide.vircle.co.kr/guarantee-setting';
+export const CARD_DESIGN_GUIDE_LINK = 'https://guide.vircle.co.kr/designguide';
 
 export const ERROR_TITLE = '일시적인 오류가 발생하였습니다.';
 export const ERROR_MESSAGE = '잠시 후 다시 시도해주세요.';
