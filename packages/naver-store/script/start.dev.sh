538298d774432f2f4c9db0d4b1e72558c0e1a7e5
massTodd <122768283+massTodd@users.noreply.github.com>
2023-03-07 15:43:16 +0900

    feat(naver-store): build test (#496)

--- packages/naver-store/script/start.dev.sh
+++ packages/naver-store/script/start.dev.sh
@@ -26,7 +26,7 @@ yarn
 
 echo ">[$(date) BUILD START]" >> $LOG_POINT
 
-yarn build >> $LOG_POINT
+yarn workspace @vircle/naver-store build >> $LOG_POINT
 
 echo ">[$(date)] Kill All pm2 process]" >> $LOG_POINT
 
